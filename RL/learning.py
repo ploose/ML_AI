86794e2bbd047fc0be2bee13986821ab6c866fa8
ploose <paeddy.lapi@gmail.com>
2022-09-17 04:12:32 +0200

    Add second project

---
+++ RL/learning.py
@@ -0,0 +1,38 @@
+from tensorflow.keras.losses import MSE
+import tensorflow as tf
+
+def calculate_loss(experiences, gamma, q_network, target_q_network):
+    """ 
+    Calculates the loss.
+
+    Args:
+      experiences: (tuple) tuple of ["state", "action", "reward", "next_state", "done"] namedtuples
+      gamma: (float) The discount factor.
+      q_network: (tf.keras.Sequential) Keras model for predicting the q_values
+      target_q_network: (tf.keras.Sequential) Karas model for predicting the targets
+
+    Returns:
+      loss: (TensorFlow Tensor(shape=(0,), dtype=int32)) the Mean-Squared Error between
+            the y targets and the Q(s,a) values.
+    """
+
+
+    # Unpack the mini-batch of experience tuples
+    states, actions, rewards, next_states, done_vals = experiences
+
+    # Compute max Q^(s,a)
+    max_qsa = tf.reduce_max(target_q_network(next_states), axis=-1)
+
+    # Set y = R if episode terminates, otherwise set y = R + γ max Q^(s,a).
+    y_targets = rewards + (gamma * max_qsa * (1 - done_vals))
+
+    # Get the q_values
+    q_values = q_network(states)
+    q_values = tf.gather_nd(q_values, tf.stack([tf.range(q_values.shape[0]),
+                                                tf.cast(actions, tf.int32)], axis=1))
+
+    # Calculate the loss
+    loss = MSE(y_targets, q_values)
+
+    return loss
+
